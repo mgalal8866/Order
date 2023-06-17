a085d45c3f665682c6c8a36e7229f10bcabc4025
mgalal <mgalal8866@gmail.com>
2023-06-17 20:17:00 +0200

    modified:   config/auth.php 	modified:   config/jwt.php

--- config/jwt.php
+++ config/jwt.php
@@ -101,7 +101,7 @@ return [
     |
     */
 
-    'ttl' => env('JWT_TTL', null),
+    'ttl' => env('JWT_TTL', 60),
 
     /*
     |--------------------------------------------------------------------------
@@ -148,7 +148,7 @@ return [
     'required_claims' => [
         'iss',
         'iat',
-        // 'exp',
+        'exp',
         'nbf',
         'sub',
         'jti',
