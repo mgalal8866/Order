7e6e528ebd345d2b7ff768c89df026b0b6f6c5e4
mgalal8866 <mgalal8866@gmail.com>
2023-06-19 00:58:29 +0300

    modified:   app/Models/ProductHeader.php 	modified:   app/Models/setting.php

--- app/Models/ProductHeader.php
+++ app/Models/ProductHeader.php
@@ -18,4 +18,5 @@ class ProductHeader extends Model
         return $this->hasMany(productdetails::class,'product_id');
     }
 
+
 }
--- app/Models/setting.php
+++ app/Models/setting.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 
 class setting extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function getLogoShopAttribute($val)
+    {
+        $path = public_path('assets/images/' . $val);
+        if (File::exists($path)) {
+            return ($val !== null) ? asset('asset/images/' . $val) : asset('asset/images/noimage.jpg');
+        } else {
+            return asset('asset/images/noimage.jpg');
+        }
+    }
 }
