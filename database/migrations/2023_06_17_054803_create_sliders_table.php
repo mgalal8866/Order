4f049adb7e4e833aed9be70da576c4988e0bae1f
mgalal8866 <mgalal8866@gmail.com>
2023-06-18 23:47:49 +0300

    modified:   database/migrations/2023_06_17_054803_create_sliders_table.php 	new file:   public/asset/images/slider/1.jpg

--- database/migrations/2023_06_17_054803_create_sliders_table.php
+++ database/migrations/2023_06_17_054803_create_sliders_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('image');
-            $table->boolean('active');
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('active')->default(1)->nullable();
             $table->timestamps();
         });
     }
