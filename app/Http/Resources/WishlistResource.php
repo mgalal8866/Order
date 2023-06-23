9709ee3ee80763f4289eb8862aa319ce9e08194f
mgalal <mgalal8866@gmail.com>
2023-06-23 16:47:09 +0200

    modified:   app/Http/Resources/WishlistResource.php

--- app/Http/Resources/WishlistResource.php
+++ app/Http/Resources/WishlistResource.php
@@ -22,7 +22,7 @@ class WishlistResource extends JsonResource
             "productd_barcode"=> $this->productdetails->productd_barcode??'',
             "productd_image" => $this->productdetails->productd_image??'',
             "productd_unit"  => $this->productdetails->productd_UnitType == 2 ?
-                $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name . ' X ' . $this->productdetails->unit->unit_name . ' = ' . $this->productdetails->productd_size : ($this->productdetails->productd_UnitType == 3 ? $units[$this->productdetails->productd_UnitType - 2]->productd_size . "X" . $this->productdetails->productd_size . "X" . $this->unit->unit_name . ' = ' . $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name :  $this->productdetails->unit->unit_name),
+                $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name . ' X ' . $this->productdetails->unit->unit_name . ' = ' . $this->productdetails->productd_size : ($this->productdetails->productd_UnitType == 3 ? $units[$this->productdetails->productd_UnitType - 2]->productd_size . "X" . $this->productdetails->productd_size . "X" . $this->unit->unit_name . ' = ' . $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name :  $this->productdetails->unit->unit_name ??''),
             "productd_Sele1" => $this->productdetails->productd_Sele1??0.00,
             "productd_Sele2" => $this->productdetails->productd_Sele2??0.00,
             "isoffer"        => $this->productdetails->isoffer,
