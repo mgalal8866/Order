3800ead51c4dde1b4d6e21be1479c0db3d6d5f72
mgalal8866 <mgalal8866@gmail.com>
2023-07-08 07:23:05 +0300

    modified:   public/front/assets/css/style.css 	modified:   public/front/assets/scss/style.scss 	new file:   resources/views/layouts/front-end/footer.blade.php 	new file:   resources/views/layouts/front-end/head.blade.php 	new file:   resources/views/layouts/front-end/header.blade.php 	modified:   resources/views/layouts/front-end/layout.blade.php 	new file:   resources/views/layouts/front-end/menumobile.blade.php 	new file:   resources/views/layouts/front-end/script.blade.php 	new file:   resources/views/layouts/front-end/sliderbr.blade.php

---
+++ resources/views/layouts/front-end/script.blade.php
@@ -0,0 +1,38 @@
+<!-- latest jquery-->
+<script src="{{asset('front/assets/js/jquery-3.6.0.min.js')}}"></script>
+
+<!-- jquery ui-->
+<script src="{{asset('front/assets/js/jquery-ui.min.js')}}"></script>
+
+<!-- Bootstrap js-->
+<script src="{{asset('front/assets/js/bootstrap/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('front/assets/js/bootstrap/bootstrap-notify.min.js')}}"></script>
+<script src="{{asset('front/assets/js/bootstrap/popper.min.js')}}"></script>
+
+<!-- feather icon js-->
+<script src="{{asset('front/assets/js/feather/feather.min.js')}}"></script>
+<script src="{{asset('front/assets/js/feather/feather-icon.js')}}"></script>
+
+<!-- Lazyload Js -->
+<script src="{{asset('front/assets/js/lazysizes.min.js')}}"></script>
+
+<!-- Slick js-->
+<script src="{{asset('front/assets/js/slick/slick.js')}}"></script>
+<script src="{{asset('front/assets/js/slick/slick-animation.min.js')}}"></script>
+<script src="{{asset('front/assets/js/slick/custom_slick.js')}}"></script>
+
+<!-- Auto Height Js -->
+<script src="{{asset('front/assets/js/auto-height.js')}}"></script>
+
+<!-- Fly Cart Js -->
+<script src="{{asset('front/assets/js/fly-cart.js')}}"></script>
+
+<!-- Quantity js -->
+<script src="{{asset('front/assets/js/quantity-2.js')}}"></script>
+
+<!-- WOW js -->
+<script src="{{asset('front/assets/js/wow.min.js')}}"></script>
+<script src="{{asset('front/assets/js/custom-wow.js')}}"></script>
+
+<!-- script js -->
+<script src="{{asset('front/assets/js/script.js')}}"></script>
---
+++ resources/views/layouts/front-end/sliderbr.blade.php
@@ -0,0 +1,30 @@
+<section class="home-search-full pt-0 overflow-hidden">
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12">
+                <div class="slider-animate">
+                    <div>
+                        <div class="home-contain rounded-0 p-0">
+                            <img src="{{asset('front/assets/images/vegetable/bg-img.jpg')}}"
+                                class="img-fluid bg-img blur-up lazyload bg-top" alt="">
+                            <div class="home-detail p-center text-center home-overlay position-relative">
+                                <div>
+                                    <div class="content">
+                                        <h1>Get your grocery in 25 minutes</h1>
+                                        <h3>Better ingredients, better food, and beverages, at low prices</h3>
+                                        <div class="search-box">
+                                            <input type="search" class="form-control"
+                                                placeholder="I'm searching for..."
+                                                aria-label="Recipient's username">
+                                            <i data-feather="search"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
