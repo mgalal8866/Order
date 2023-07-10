ba0e045c15e5fe8baf944d0d0c7968a13c9ca2f2
mgalal8866 <mgalal8866@gmail.com>
2023-07-10 08:21:29 +0300

    modified:   resources/views/livewire/dashboard/notification/view-notification.blade.php

--- resources/views/livewire/dashboard/notification/view-notification.blade.php
+++ resources/views/livewire/dashboard/notification/view-notification.blade.php
@@ -374,7 +374,7 @@
                                         </span></span></span></div>
                         </div>
                     @endif
-                    <div   class="col-12 col-md-6 ">
+                    {{-- <div   class="col-12 col-md-6 ">
                         <label class="form-label" for="first-name-icon">{{__('tran.customer')}}</label>
                         <div wire:ignore class=" d-flex " style="flex-warp:nwrap !important">
                             <span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>
@@ -387,7 +387,7 @@
                             <button class="btn btn-outline-primary waves-effect" data-bs-target="#newcustomer"  data-bs-toggle="modal" id="button-addon2" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></button>
                         </div>
 
-                    </div>
+                    </div> --}}
                     <div class="col-12 col-md-6">
                         <x-label for="title" label="{{ __('tran.title') }}" />
                         <input type="text" wire:model.defer='title' id="title" class="form-control" />
