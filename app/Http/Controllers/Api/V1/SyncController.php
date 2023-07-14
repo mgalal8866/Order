82a72f519cce0e4e059f90db0922564cdb3c1f80
mgalal8866 <mgalal8866@gmail.com>
2023-07-14 07:02:25 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -1,56 +1,68 @@
 <?php
 
 namespace App\Http\Controllers\Api\V1;
-use App\Http\Controllers\Controller;
+
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class SyncController extends Controller
 {
-    function client(Request $request) {
+    function client(Request $request)
+    {
         try {
+            Log::warning($request->all());
+            foreach ($request->all() as $index => $item) {
 
-            // $this->buildXMLHeader();
+                $rules['Client_fhoneWhats'] = 'unique:users';
+                $messages = [
+                    'required'  => 'error required (:attribute)',
+                    'unique'    => 'error unique (:attribute)',
+                ];
+                $validator = Validator::make($item, $rules,$messages);
+                if ($validator->fails()) {
+                    $errors[$index] = ['message'=> $validator->messages(), 'Client_id'=> $item['Client_id'] ];
+                    continue;
+                }
+                    $user = User::create([
+                        'client_fhonewhats'  => $item['Client_fhoneWhats'],
+                        'source_id'          => $item['Client_id'],
+                        'client_name'        => $item['Client_name'],
+                        'client_Balanc'      => $item['Client_Balanc'],
+                        'client_points'      => $item['Client_points'],
+                        'client_fhoneLeter'  => $item['Client_fhoneLeter'],
+                        'client_EntiteNumber' => $item['Client_EntiteNumber'],
+                        'region_id'          => $item['Region_id'],
+                        'lat_mab'            => $item['Lat_mab'],
+                        'long_mab'           => $item['Long_mab'],
+                        'client_state'       => $item['Client_state'],
+                        'client_Credit_Limit' => $item['Client_Credit_Limit'],
+                        'default_Sael'       => $item['default_Sael'],
+                        'client_note'        => $item['Client_note'],
+                        'client_code'        => $item['Client_code'],
+                        'categoryAPP'        => $item['CategoryAPP'],
+                        'client_Active'      => $item['Client_Active'],
+                        'created_at'         => $item['caret_data']
+                    ]);
+                    $results[$index] = ['id' => $user->id, 'source_id' => $user->source_id];
+            }
 
+            $data = ['users_online' =>   User::where('source_id', null)->get() ?? [], 'results' => $results ?? [], 'errors' => $errors??[]];
+            return  $data;
+        } catch (\Exception $e) {
 
-        Log::error($request->all());
-        foreach($request->all() as $index=>$item){
-
-            $user = User::create([
-            'client_fhonewhats'  =>$item['Client_fhoneWhats'],
-            'source_id'          =>$item['Client_id'],
-            'client_name'        =>$item['Client_name'],
-            'client_Balanc'      =>$item['Client_Balanc'],
-            'client_points'      =>$item['Client_points'],
-            'client_fhoneLeter'  =>$item['Client_fhoneLeter'],
-            'client_EntiteNumber'=>$item['Client_EntiteNumber'],
-            'region_id'          =>$item['Region_id'],
-            'lat_mab'            =>$item['Lat_mab'],
-            'long_mab'           =>$item['Long_mab'],
-            'client_state'       =>$item['Client_state'],
-            'client_Credit_Limit'=>$item['Client_Credit_Limit'],
-            'default_Sael'       =>$item['default_Sael'],
-            'client_note'        =>$item['Client_note'],
-            'client_code'        =>$item['Client_code'],
-            'categoryAPP'        =>$item['CategoryAPP'],
-            'client_Active'        =>$item['Client_Active'],
-            'created_at'         =>$item['caret_data']]);
-            $results[$index] =['id'=> $user->id, 'source_id' => $user->source_id] ;
+            Log::error($e->getMessage());
         }
-        $data = ['users_online'=>User::where('source_id', null)->get(), 'results'=> $results];
-        return  $data;
-    } catch (\Exception $e) {
-
-        Log::error(  $e->getMessage());
-    }
     }
 
-    function clienttest() {
-            return array(['id'=>'1']);
+    function clienttest()
+    {
+        return array(['id' => '1']);
     }
-    function categoryapp(Request $request) {
-
+    function categoryapp(Request $request)
+    {
     }
 }
