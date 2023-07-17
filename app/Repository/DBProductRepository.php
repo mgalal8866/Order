7382dfc9f42d5bb02ff0a4eb47a4b35bf15f3730
mgalal8866 <mgalal8866@gmail.com>
2023-07-17 07:53:23 +0300

    modified:   app/Repository/DBProductRepository.php

--- app/Repository/DBProductRepository.php
+++ app/Repository/DBProductRepository.php
@@ -13,22 +13,23 @@ class DBProductRepository implements ProductRepositoryinterface
     public function getprobycat($id)
     {
         // \DB::enableQueryLog(); // Enable query log
-        return Resp(new ProductCollectionResource(ProductDetails::online()->Getcategory($id)->paginate(10)), 'success', 200, true)->getData(true);
+        return Resp(new ProductCollectionResource(ProductDetails::Getcategory($id)->online()->paginate(10)), 'success', 200, true)->getData(true);
         // dd(\DB::getQueryLog()); // Show results of log
     }
     public function getoffers()
     {
         // \DB::enableQueryLog(); // Enable query log
-        return Resp(new ProductCollectionResource(ProductDetails::online()->Getoffers()->paginate(10)), 'success', 200, true)->getData(true);
+        return Resp(new ProductCollectionResource(ProductDetails:: Getoffers()->online()->paginate(10)), 'success', 200, true)->getData(true);
         // \DB::getQueryLog(); // Show results of log
     }
     public function searchproduct($search)
     {
-$results = ProductDetails::where('productd_barcode', 'LIKE', $search )
-        ->orWhereHas('productheader', function ($query) use ($search) {
-            $query->online()->where('product_name', 'LIKE', '%' . $search . '%');
-        })->online()
-        ->paginate(10);
-        return Resp(new ProductCollectionResource( $results ), 'success', 200, true)->getData(true);
+        // return $search;
+        $results = ProductDetails::where('productd_barcode', 'LIKE', $search)
+            ->orWhereHas('productheader', function ($query) use ($search) {
+                $query->online()->where('product_name', 'LIKE', "'%" . $search . "%'");
+            })->online()
+            ->paginate(10);
+        return Resp(new ProductCollectionResource($results), 'success', 200, true)->getData(true);
     }
 }
