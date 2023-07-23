4ab358ba90a807785f93bb9564231d98cbe2b5e6
mgalal8866 <mgalal8866@gmail.com>
2023-07-23 05:40:15 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -329,6 +329,7 @@ class SyncController extends Controller
                 ]);
                 DeliveryDetails::where( 'sale_header_id' ,$request[0][0]['SalesHeader_ID'])->delete();
                 foreach ($request[1] as $index => $item) {
+                    Log::info('DeliveryHeader', $item);
                     $uu =   DeliveryDetails::updateOrCreate(['sale_header_id' =>  $request[0][0]['SalesHeader_ID']], [
                         'sale_header_id'     => $item['SalesHeader_ID'],
                         'product_details_id' => $item['ProductDetails_ID'],
