8ead0f1c361d83b5bb122844dfa0a9d9bef3e2a7
mgalal8866 <mgalal8866@gmail.com>
2023-07-27 21:44:49 +0300

    modified:   app/Repository/DBUserDeliveryRepository.php

--- app/Repository/DBUserDeliveryRepository.php
+++ app/Repository/DBUserDeliveryRepository.php
@@ -76,14 +76,11 @@ class DBUserDeliveryRepository implements UserDeliveryRepositoryinterface
     }
     public function updatemap($request)
     {
-
-dd(Auth::guard('delivery')->user());
       $userddelivery =   UserDelivery::where('id',Auth::guard('delivery')->user()->id)->update(['lat'=>$request->lat,'long'=>$request->long]);
       if($userddelivery){
           Resp( $userddelivery   ,'success',200,true);
         }else{
             Resp( '','error',401,false);
-
         }
     }
     public function checkblock($request)
