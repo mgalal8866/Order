2217b8303acde8f2254d41a0fddc3026f9ebefec
mgalal8866 <mgalal8866@gmail.com>
2023-07-31 21:30:22 +0300

    modified:   app/Models/CateoryApp.php

--- app/Models/CateoryApp.php
+++ app/Models/CateoryApp.php
@@ -6,7 +6,7 @@ use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-
+use Illuminate\Support\Facades\File;
 class CateoryApp extends Model
 {
     use HasFactory, UUID;
