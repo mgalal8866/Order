f2fe05c563cd05368f051503d88a159e06c19221
mgalal8866 <mgalal8866@gmail.com>
2023-08-01 08:50:29 +0300

    modified:   app/Providers/RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,8 +46,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware(['tenant', 'web'])
                 ->group(base_path('routes/web.php'));
 
-            // Route::middleware(['web'])
-            //     ->group(base_path('routes/main.php'));
+            Route::middleware(['web'])
+                ->group(base_path('routes/main.php'));
         });
     }
 }
