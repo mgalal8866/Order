a002af781e4cc1f6aac2314cd6a2347c6eed1a98
mgalal8866 <mgalal8866@gmail.com>
2023-08-04 04:33:38 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -425,7 +425,7 @@ class SyncController extends Controller
     function downsdeliveryheader(Request $request)
     {
         $data = DeliveryHeader::where('lastsyncdate', null)->with('salesdetails')->get();
-        DeliveryHeader::query()->where('lastsyncdate', null)->update(['lastsyncdate' => carbon::now()]);
+        // DeliveryHeader::query()->where('lastsyncdate', null)->update(['lastsyncdate' => carbon::now()]);
         return    Resp(DeliveryHeaderResource::collection( $data), 'success', 200, true);
     }
     function downdeliverydetails(Request $request)
