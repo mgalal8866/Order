dd4e360a4aa4f27d973bb685d2d929e7f75709f5
mgalal8866 <mgalal8866@gmail.com>
2023-08-04 06:13:32 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -443,7 +443,7 @@ class SyncController extends Controller
     function sendnotification(Request $request)
     {
 
-        Log::error('uploadcategoryapp', $request[0]);
+        Log::error('uploadcategoryapp', $request);
         // Log::info('uploadcategoryapp', json_encode($request[0],JSON_UNESCAPED_SLASHES));
         // Log::info('uploadcategoryapp', json_decode($request));
         // Log::info('uploadcategoryapp', response()->json($request->all(), 200, [], JSON_UNESCAPED_UNICODE));
