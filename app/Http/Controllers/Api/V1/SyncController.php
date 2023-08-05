62548a11c4e8e53b8d6fbcfc046872cb53f835ae
mgalal8866 <mgalal8866@gmail.com>
2023-08-05 04:30:56 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -445,6 +445,7 @@ class SyncController extends Controller
         $dd = json_decode($request[0], true);
         Log::info('notification',$dd);
         $image = $dd['image'] != null ? uploadbase64images('notification', $dd['image']) : null;
+        Log::error( $image);
         $result = notificationFCM($dd['title'], $dd['body'], $dd['users'],null,'https://elshrouk.order-bay.com/asset/images/notification/'.$image);
         // $notifi =   // return    Resp($notifi , 'success', 200, true);
 
