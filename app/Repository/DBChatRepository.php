8a0aa08991e8f1f4bc6a021ded24f8dc6e229982
mgalal8866 <mgalal8866@gmail.com>
2023-08-07 01:40:07 +0300

    modified:   app/Models/Message.php 	modified:   app/Repository/DBChatRepository.php

--- app/Repository/DBChatRepository.php
+++ app/Repository/DBChatRepository.php
@@ -2,15 +2,14 @@
 
 namespace App\Repository;
 
+use App\Http\Resources\chat\ChatResource;
 use Carbon\Carbon;
 use App\Models\Cart;
-use App\Models\Message;
-use App\Models\deferred;
 use App\Models\conversion;
-use App\Events\MessageSent;
+use App\Models\deferred;
+use App\Models\Message;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Resources\chat\ChatResource;
 use App\Repositoryinterface\ChatRepositoryinterface;
 
 class DBChatRepository implements ChatRepositoryinterface
@@ -19,21 +18,11 @@ class DBChatRepository implements ChatRepositoryinterface
     {
         $conversion = conversion::where('client_id', Auth::guard('api')->user()->id)->first();
         if ($conversion) {
-<<<<<<< HEAD
              $messages =  Message::create(['conversions_id'=> $conversion->id,'message' => $message, 'client_id' => Auth::guard('api')->user()->id]);;
             return new ChatResource($messages);
             }else{
             $conversion = conversion::create(['client_id'=> Auth::guard('api')->user()->id]);
             $messages = Message::create(['conversions_id' => $conversion->id, 'message' => $message, 'client_id' => Auth::guard('api')->user()->id]);;
-=======
-             $messages =  message::create(['conversions_id'=> $conversion->id,'message' => $message, 'client_id' => Auth::guard('api')->user()->id]);;
-
-            return new ChatResource($messages);
-            }else{
-            $conversion = conversion::create(['client_id'=> Auth::guard('api')->user()->id]);
-            $messages = message::create(['conversions_id' => $conversion->id, 'message' => $message, 'client_id' => Auth::guard('api')->user()->id]);;
-
->>>>>>> cbd55b68f339319495030a0d2d3e9cb7d689c70f
             return new ChatResource($messages);
         }
     }
