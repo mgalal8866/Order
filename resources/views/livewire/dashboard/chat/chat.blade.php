a1cdf46b31703d0c4662d92c6abe63ea154b6b56
mgalal8866 <mgalal8866@gmail.com>
2023-08-10 05:36:30 +0300

    modified:   resources/views/livewire/dashboard/chat/chat.blade.php

--- resources/views/livewire/dashboard/chat/chat.blade.php
+++ resources/views/livewire/dashboard/chat/chat.blade.php
@@ -183,7 +183,7 @@
                                 <header class="chat-header">
                                     <div class="d-flex align-items-center">
                                         <div class="sidebar-toggle d-block d-lg-none me-1">
-                                            <i data-feather="menu" class="font-medium-5"></i>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu ficon"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                                         </div>
                                         <div class="avatar avatar-border user-profile-toggle m-0 me-1">
                                             <img src="{{ Avatar::create($nameuser)->setFontFamily('Cairo')->toBase64() }}"
