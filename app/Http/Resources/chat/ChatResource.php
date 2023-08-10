cd751476179b15e5df82c0754cf09afc67b65600
mgalal8866 <mgalal8866@gmail.com>
2023-08-10 06:06:09 +0300

    modified:   app/Http/Resources/chat/ChatResource.php

--- app/Http/Resources/chat/ChatResource.php
+++ app/Http/Resources/chat/ChatResource.php
@@ -21,7 +21,7 @@ class ChatResource extends JsonResource
             "admin_idsite"          => $this->admin_id,
             "message"               => $this->message ?? '',
             "seen"                  => $this->seen,
-            "sendername"            => $this->user->client_name ?? 'admin -'. $this->admin->employee->name,
+            "sendername"            => $this->user->client_name ??   $this->admin->employee->name,
             "datetime"              => Carbon::parse($this->created_at)->translatedFormat('H:i A / m-d-Y') ?? '',
             "created_at"            => $this->created_at?? '',
         ];
