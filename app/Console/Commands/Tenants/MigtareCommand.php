9452bfe082f32c7497a2bdca9600994191b22a54
mgalal8866 <mgalal8866@gmail.com>
2023-08-11 01:38:29 +0300

    modified:   app/Console/Commands/Tenants/MigtareCommand.php

--- app/Console/Commands/Tenants/MigtareCommand.php
+++ app/Console/Commands/Tenants/MigtareCommand.php
@@ -35,7 +35,8 @@ class MigtareCommand extends Command
             Tenants::switchToTanent($tenant);
             $this->info('Start migrating : '.$tenant->domain);
             $this->info('---------------------------------------');
-            Artisan::call('migrate --path=database/migrations/tenants/  --database=tenant');
+            // Artisan::call('migrate --path=database/migrations/tenants/  --database=tenant');
+            Artisan::call('migrate --path=database/migrations/  --database=tenant');
             // \Artisan::call('migrate:rollback --path=database/migrations/tenants/ --database=tenant' );
             $this->info(Artisan::output());
         });
