796ae523e38fb3deadd6eae6d01ef633817e3b5b
mgalal8866 <mgalal8866@gmail.com>
2023-08-13 23:05:54 +0300

    modified:   app/Http/Resources/CartResource.php 	modified:   database/migrations/tenants/2014_10_12_000000_create_users_table.php

--- database/migrations/tenants/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenants/2014_10_12_000000_create_users_table.php
@@ -18,27 +18,27 @@ return new class extends Migration
             $table->string('password')->nullable();
             $table->string('source_id')->nullable();
             $table->string('client_name')->nullable();
-            $table->decimal('client_Balanc',8,2)->default(0);
-            $table->decimal('client_points',8,2)->default(0);
+            $table->decimal('client_Balanc', 8, 2)->default(0);
+            $table->decimal('client_points', 8, 2)->default(0);
             $table->string('client_fhoneLeter')->nullable();
             $table->string('client_EntiteNumber')->nullable();
             $table->integer('region_id')->nullable();
             $table->string('store_name')->nullable();
             $table->string('lat_mab')->nullable();
             $table->string('long_mab')->nullable();
-            $table->string('client_state')->nullable();//عنوان
-            $table->decimal('client_Credit_Limit',8,2)->default(0);//الحد الائتمانى
-            $table->string('default_Sael')->default('كاش');//نوع التعامل كاش وتقسيط
+            $table->string('client_state')->nullable(); //عنوان
+            $table->decimal('client_Credit_Limit', 8, 2)->default(0); //الحد الائتمانى
+            $table->string('default_Sael')->default('كاش'); //نوع التعامل كاش وتقسيط
             $table->text('client_note')->nullable();
             $table->boolean('client_Active')->default(1);
             $table->string('client_code')->nullable();
+            $table->string('code_client')->nullable();
             $table->integer('categoryAPP')->nullable();
             $table->integer('source_type')->nullable();
             $table->string('last_active')->nullable();
             $table->timestamp('last_seen')->nullable();
             $table->rememberToken();
             $table->timestamps();
-
         });
     }
 
