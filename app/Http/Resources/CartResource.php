1dbd2d1de89361e34b4569093af0940c8c125628
mgalal8866 <mgalal8866@gmail.com>
2023-08-13 22:34:44 +0300

    modified:   app/Http/Resources/CartResource.php

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -12,20 +12,20 @@ class CartResource extends JsonResource
 
     public function toArray(Request $request): array
     {
-     
+
         $units = $this->productdetails->units($this->productdetails->product_header_id)->get();
         return [
             "conver"        => $this->id,
             "productd_id"    => $this->productdetails->id,
             "productd_name"  => $this->productdetails->productheader->product_name ?? '',
-            "productd_qty"   => $this->qty ?? '',
+            "productd_qty"   =>  $this->productdetails->productheader->product_isscale==1? number_format((float)$this->qty, 2, '.', '') : (int)$this->qty,
             "productd_bay"   => $this->productdetails->productd_bay ?? '',
             "product_isscale" => $this->productdetails->productheader->product_isscale,
             "productd_barcode" => $this->productdetails->productd_barcode ?? '',
             "productd_image" => $this->productdetails->productd_image ?? '',
             "productd_unit"  => $this->productdetails->productd_UnitType == 2 ?
                 $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name . ' X ' . $this->productdetails->unit->unit_name . ' = ' . $this->productdetails->productd_size : ($this->productdetails->productd_UnitType == 3 ? $units[$this->productdetails->productd_UnitType - 2]->productd_size . "X" . $this->productdetails->productd_size . "X" . $this->productdetails->unit->unit_name . ' = ' . $units[$this->productdetails->productd_UnitType - 2]->unit->unit_name :  $this->productdetails->unit->unit_name ?? ''),
-            "productd_Sele1" => $this->productdetails->productd_Sele1 ?? 0.00,
+            "productd_Sele1" =>$this->productdetails->productd_Sele1 ?? 0.00,
             "productd_Sele2" => $this->productdetails->productd_Sele2 ?? 0.00,
             "isoffer"        => $this->productdetails->isoffer,
             "maxqty"         => $this->productdetails->maxqty ?? 0,
