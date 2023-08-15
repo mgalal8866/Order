087dee7cbefe78c57c9a37c13539a301b87d1db7
mgalal8866 <mgalal8866@gmail.com>
2023-08-15 04:48:30 +0300

    modified:   app/Console/Commands/Tenants/seadercommand.php

--- app/Console/Commands/Tenants/seadercommand.php
+++ app/Console/Commands/Tenants/seadercommand.php
@@ -29,7 +29,6 @@ class seadercommand extends Command
     public function handle()
     {
         $class = $this->argument('class');
-        dd( $class);
         $tenants = Tenant::get();
         $tenants->each(function ($tenant) use ($class) {
             Tenants::switchToTanent($tenant);
