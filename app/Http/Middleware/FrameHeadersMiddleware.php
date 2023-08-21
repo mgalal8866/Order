628836a7f0529e3236c5803946abfd27b0b08512
mgalal8866 <mgalal8866@gmail.com>
2023-08-21 16:56:38 +0300

    modified:   app/Http/Kernel.php 	new file:   app/Http/Middleware/FrameHeadersMiddleware.php

---
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FrameHeadersMiddleware
+{
+
+
+    public function handle($request, Closure $next)
+    {
+         $response = $next($request);
+         $response->header('X-Frame-Options', 'ALLOW FROM https://www.medawee.com/');
+         return $response;
+     }
+}
