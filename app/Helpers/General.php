c94e23b043b45414976753dd2a071e03664cbaeb
Mohamed Galal <58759061+mgalal8866@users.noreply.github.com>
2023-08-22 18:08:23 +0300

    Update General.php

--- app/Helpers/General.php
+++ app/Helpers/General.php
@@ -27,9 +27,9 @@ function Resp($data = null , $msg = null , $status = 200 ,$statusval=true){
     $code = rand(123456, 999999);
     $msg = 'كود التحقق ' . $code;
     $response = Http::accept('application/json')->post('https://smssmartegypt.com/sms/api/',[
-    username=>.env('SMS_USERNAME','gomla1234'),
-    password=>.env('SMS_PASSWORD','Gomla1234'),
-    sendername=>.env('SMS_SENDERID','test'),
+    username=>env('SMS_USERNAME','gomla1234'),
+    password=>env('SMS_PASSWORD','Gomla1234'),
+    sendername=>env('SMS_SENDERID','test'),
     message=>$msg,
     mobiles=>01024346011]);
    // &mobiles='.$phone);
