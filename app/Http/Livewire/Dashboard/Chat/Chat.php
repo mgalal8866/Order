6ebdbafb05176947b496732b549cb67f43f105f9
mgalal8866 <mgalal8866@gmail.com>
2023-08-28 07:46:02 +0300

    modified:   app/Http/Livewire/Dashboard/Chat/Chat.php 	modified:   config/broadcasting.php

--- app/Http/Livewire/Dashboard/Chat/Chat.php
+++ app/Http/Livewire/Dashboard/Chat/Chat.php
@@ -17,12 +17,12 @@ class Chat extends Component
     public function sentmessage(){
         $messages = message::create(['seenmsg' =>  0, 'conversions_id' =>  $this->conversions_id, 'message' => $this->text, 'admin_id' => Auth::guard('admin')->user()->id]);;
 
+        $con = conversion::find($this->conversions_id);
+        $sett = setting::find(1);
+        notificationFCM('رسالة جديده', $this->text,[$con->user->fsm]);
         $msg = new ChatResource($messages);
          event(new MessageSent($msg));
          $this->text ='';
-         $con = conversion::find($this->conversions_id);
-         $sett = setting::find(1);
-         notificationFCM('رسالة جديده', $this->text,[$con->user->fsm]);
     }
     public function loadmessage($id,$name){
         $this->conversions_id = $id;
