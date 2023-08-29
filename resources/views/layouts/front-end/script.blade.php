c1a0d686202bc7fb728472c492ab49e03dfe735c
mgalal8866 <mgalal8866@gmail.com>
2023-08-30 00:36:00 +0300

    modified:   resources/views/layouts/front-end/script.blade.php 	modified:   resources/views/livewire/front/cart/cart.blade.php

--- resources/views/layouts/front-end/script.blade.php
+++ resources/views/layouts/front-end/script.blade.php
@@ -37,5 +37,44 @@
 <!-- script js -->
 <script src="{{asset('front/assets/js/script.js')}}"></script>
 @livewireScripts()
+  <script>
+        window.addEventListener('notifi', event => {
+            $.notify({
+                icon: "fa fa-check",
+                // title: "Success!",
+                message: event.detail.message,
+            }, {
+                element: "body",
+                position: null,
+                type: event.detail.type,
+                allow_dismiss: true,
+                newest_on_top: true,
+                showProgressbar: true,
+                placement: {
+                    from: "top",
+                    align: "center",
+                },
+                offset: 20,
+                spacing: 10,
+                z_index: 1031,
+                delay: 600,
+                animate: {
+                    enter: "animated fadeInDown",
+                    exit: "animated fadeOutUp",
+                },
+                icon_type: "class",
+                template: '<div data-notify="container" class="col-xxl-3 col-lg-5 col-md-6 col-sm-7 col-12 alert alert-{0}" role="alert">' +
+                    '<button type="button" aria-hidden="true" class="btn-close" data-notify="dismiss"></button>' +
+                    '<span data-notify="icon"></span> ' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<div class="progress" data-notify="progressbar">' +
+                    '<div class="progress-bar progress-bar-info progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                    "</div>" +
+                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                    "</div>",
+            });
+        })
+    </script>
 @stack('jslive')
 
