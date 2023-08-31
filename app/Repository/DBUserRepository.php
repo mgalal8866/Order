0ee37523815dd2e008da0ef6bcd2018b26a814a0
mgalal8866 <mgalal8866@gmail.com>
2023-08-31 21:31:35 +0300

    modified:   app/Repository/DBUserRepository.php

--- app/Repository/DBUserRepository.php
+++ app/Repository/DBUserRepository.php
@@ -16,19 +16,19 @@ class DBUserRepository implements UserRepositoryinterface
     public function sendotp($phone)
     {
         $response = sendsms($phone);
-        if( $response  == 1){
+        if ($response  == 1) {
             return Resp('', 'تم ارسال كود التحقق', 200, true);
-        }else{
+        } else {
             return Resp('', 'خطاء فى ارسال كود التحقق', 302, false);
         }
     }
     public function verificationcode($request)
     {
-        $response = otp_check($request->get('client_fhonewhats'),$request->code  );
+        $response = otp_check($request->get('client_fhonewhats'), $request->code);
 
-        if( $response === 1){
+        if ($response === 1) {
             return $this->login($request);
-        }else{
+        } else {
             return Resp('', 'كود التحقق خطاء', 302, false);
         }
     }
@@ -49,7 +49,7 @@ class DBUserRepository implements UserRepositoryinterface
         $text = getsetting()->notif_welcome_text;
         // Log::error($user);
         $rep = replacetext($text, $user);
-        notificationFCM('اهلا بك', $rep,[$user->fsm]);
+        notificationFCM('اهلا بك', $rep, [$user->fsm]);
         return Resp($data, 'Success', 200, true);
     }
     public function edit($request)
@@ -57,16 +57,16 @@ class DBUserRepository implements UserRepositoryinterface
         DB::beginTransaction();
         try {
             $user =  User::find(Auth::guard('api')->user()->id);
-            $user->client_name       = $request['client_name']??$user->client_name ;
-            $user->client_fhoneLeter = $request['client_fhoneLeter']??$user->client_fhoneLeter ;
-            $user->region_id         = $request['region_id']??$user->region_id         ;
-            $user->store_name        = $request['store_name']??$user->store_name        ;
-            $user->lat_mab           = $request['lat_mab']??$user->lat_mab           ;
-            $user->long_mab          = $request['long_mab']??$user->long_mab          ;
-            $user->client_state      = $request['client_state']??$user->client_state      ;
-            $user->CategoryAPP       = $request['CategoryAPP']??$user->CategoryAPP       ;
-            $user->client_code       = $request['client_code']??$user->client_code       ;
-            $user->store_name        = $request['store_name']??$user->store_name        ;
+            $user->client_name       = $request['client_name'] ?? $user->client_name;
+            $user->client_fhoneLeter = $request['client_fhoneLeter'] ?? $user->client_fhoneLeter;
+            $user->region_id         = $request['region_id'] ?? $user->region_id;
+            $user->store_name        = $request['store_name'] ?? $user->store_name;
+            $user->lat_mab           = $request['lat_mab'] ?? $user->lat_mab;
+            $user->long_mab          = $request['long_mab'] ?? $user->long_mab;
+            $user->client_state      = $request['client_state'] ?? $user->client_state;
+            $user->CategoryAPP       = $request['CategoryAPP'] ?? $user->CategoryAPP;
+            $user->client_code       = $request['client_code'] ?? $user->client_code;
+            $user->store_name        = $request['store_name'] ?? $user->store_name;
             $user->save();
             $data =  new UserResource($user);
             return Resp($data, 'Success', 200, true);
@@ -83,19 +83,20 @@ class DBUserRepository implements UserRepositoryinterface
     {
         Log::warning($request);
         $user = User::create([
-        'client_name'=>$request['client_name']??null,
-        'client_fhonewhats'=>$request['client_fhonewhats']??null,
-        'client_fhoneLeter'=>$request['client_fhoneLeter']??null,
-        'region_id'=>$request['region_id']??null,
-        'lat_mab'=>$request['lat_mab']??null,
-        'long_mab'=>$request['long_mab']??null,
-        'client_state'=>$request['client_state']??null,
-        'long_mab'=>$request['long_mab']??null,
-        'CategoryAPP'=>$request['CategoryAPP']??null,
-        'client_code'=>$request['client_code']??null,
-        'store_name' =>$request['store_name']??null]);
+            'client_name' => $request['client_name'] ?? null,
+            'client_fhonewhats' => $request['client_fhonewhats'] ?? null,
+            'client_fhoneLeter' => $request['client_fhoneLeter'] ?? null,
+            'region_id' => $request['region_id'] ?? null,
+            'lat_mab' => $request['lat_mab'] ?? null,
+            'long_mab' => $request['long_mab'] ?? null,
+            'client_state' => $request['client_state'] ?? null,
+            'long_mab' => $request['long_mab'] ?? null,
+            'CategoryAPP' => $request['CategoryAPP'] ?? null,
+            'client_code' => $request['client_code'] ?? null,
+            'store_name' => $request['store_name'] ?? null
+        ]);
 
-        if (!$token =auth('api')->login($user)) {
+        if (!$token = auth('api')->login($user)) {
             return Resp(null, 'Unauthorized', 404, false);
         }
         $user->token = $token;
@@ -118,10 +119,11 @@ class DBUserRepository implements UserRepositoryinterface
     }
     public function sendtoken($token)
     {
-    //    $uu =  Auth::guard('api')->user();
+        //    $uu =  Auth::guard('api')->user();
 
-    //    Log::error($token);
-        auth('api')->user()->update(['fsm' =>$token]);
+        //    Log::error($token);
+        $user = User::find(auth('api')->user()->id);
+        $user->update(['fsm' => $token]);
         return response()->json(['Token successfully stored.']);
     }
     public function logout()
