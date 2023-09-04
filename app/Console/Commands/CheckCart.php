faf29b7cdd3b73afee587c58751fa1be9d624e0f
mgalal8866 <mgalal8866@gmail.com>
2023-09-04 05:21:23 +0300

    modified:   app/Console/Commands/CheckCart.php

--- app/Console/Commands/CheckCart.php
+++ app/Console/Commands/CheckCart.php
@@ -37,14 +37,15 @@ class CheckCart extends Command
         $tenants->each(
             function ($tenant) use($tenants) {
                 $set = Cache::get($tenant->domin . '_settings', []);
-                if ( $set['notif_sent_cart'] == 1) {
+                Log::error( $set);
+                if ( $set->notif_sent_cart == 1) {
                     Tenants::switchToTanent($tenant);
                     $from = Carbon::now()->subMinutes(5); // 2023-09-04 03:05:44
                     $to = Carbon::now(); // 2023-09-04 03:10:44
                     $users = User::on('tenant')->wherehas('cart', function ($q) use($from,$to) {
                         $q->whereBetween('updated_at', [$from, $to]);
                     })->where('fsm','!=',null)->pluck('fsm');
-                    notificationFCM('مرحبا', $set['notif_cart_text'], $users,null,null,null,null,null,false);
+                    notificationFCM('مرحبا', $set->notif_cart_text, $users,null,null,null,null,null,false);
                 }
                 Log::alert("done Cron job",[]);
             }
