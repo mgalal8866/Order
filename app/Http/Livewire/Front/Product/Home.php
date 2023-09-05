63a38ef297156c76c779865cf1620e9b31d144d6
mgalal8866 <mgalal8866@gmail.com>
2023-09-05 05:54:03 +0300

    modified:   app/Http/Livewire/Front/Product/Home.php

--- app/Http/Livewire/Front/Product/Home.php
+++ app/Http/Livewire/Front/Product/Home.php
@@ -24,7 +24,14 @@ class Home extends Component
         $this->count +=30;
     }
     public function selectid($id)  {
-        $this->idcategory = $id;
+        
+        $products  = ProductDetails::online()->Getcategory($id)->with('productheader')->with('unit')->with('cart'
+        ,function($q){
+            if(!empty(Auth::guard('client')->user()->id)){
+                $q->where('user_id',Auth::guard('client')->user()->id);
+            }
+        })->orderBy('updated_at','DESC')->paginate($this->count);
+        $this->data['products'] = $products ;
 
     }
     public function render()
