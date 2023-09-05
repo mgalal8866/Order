e4f2230bd6a33e2d793053c2c8436ec8d47c48bf
mgalal8866 <mgalal8866@gmail.com>
2023-09-05 06:43:42 +0300

    modified:   resources/views/livewire/front/category/viewcategory.blade.php

--- resources/views/livewire/front/category/viewcategory.blade.php
+++ resources/views/livewire/front/category/viewcategory.blade.php
@@ -10,7 +10,7 @@
                                 <div class="category-list">
                                     {{-- <img src="{{asset('front/assets/svg/1/vegetable.svg')}}" class="blur-up lazyload" alt=""> --}}
                                     <h5>
-                                        <a href="{{ route('categoryproduct', ['categoryid' => null]) }}" >الجميع</a>
+                                        <a href="{{ route('categoryproduct', ['categoryid' => null]) }}">الجميع</a>
                                         {{-- <a wire:click.prevent="selectid(null)" href="">الجميع</a> --}}
                                     </h5>
                                 </div>
@@ -20,7 +20,8 @@
                                     <div wire:ignore.self class="category-list">
                                         {{-- <img src="{{asset('front/assets/svg/1/vegetable.svg')}}" class="blur-up lazyload" alt=""> --}}
                                         <h5>
-                                             <a href="{{ route('categoryproduct', ['categoryid' => $category->id]) }}" >{{ $category->category_name }}</a>
+                                            <a
+                                                href="{{ route('categoryproduct', ['categoryid' => $category->id]) }}">{{ $category->category_name }}</a>
 
                                             {{-- <a wire:click.prevent="selectid({{ $category->id }})" href="">{{ $category->category_name }}</a> --}}
                                         </h5>
@@ -33,17 +34,15 @@
                 @empty(!$data['products'])
                     <div class="col-xxl-9 col-lg-8">
                         <div class="title d-block">
-                            <h2 class="text-theme font-sm">{{$cat->category_name?? ($categoryid == null?'الكل':'قسم غير موجود')}}</h2>
+                            <h2 class="text-theme font-sm">
+                                {{ $cat->category_name ?? ($categoryid == null ? 'الكل' : 'قسم غير موجود') }}</h2>
                         </div>
-                        <div
-                            class="row row-cols-xxl-5 row-cols-xl-6 row-cols-md-3 row-cols-2 g-sm-4 g-3 no-arrow
-                        section-b-space">
-
+                        <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-md-3 row-cols-2 g-sm-4 g-3 no-arrow section-b-space">
                             @forelse ($data['products'] as $product)
-                            @livewire('front.compon.product', ['product' => $product], key($product->id))
-                                @empty
+                                @livewire('front.compon.product', ['product' => $product], key($product->id))
+                            @empty
                                 <div>
-                                  <h3>لايوجد منتجات حاليا</h3>
+                                    <h3>لايوجد منتجات حاليا</h3>
                                 </div>
                             @endforelse
                         </div>
@@ -52,5 +51,4 @@
             </div>
         </div>
     </section>
-
 </div>
