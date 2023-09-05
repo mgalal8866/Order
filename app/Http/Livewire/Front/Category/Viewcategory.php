cc3f8e86144c62cb698d485853c79018eb0a282c
mgalal8866 <mgalal8866@gmail.com>
2023-09-05 06:28:08 +0300

    modified:   app/Http/Livewire/Front/Category/Viewcategory.php

--- app/Http/Livewire/Front/Category/Viewcategory.php
+++ app/Http/Livewire/Front/Category/Viewcategory.php
@@ -12,14 +12,12 @@ class Viewcategory extends Component
     use WithPagination;
     public $categoryid,$pg=30,$cat ;
     public function mount($categoryid=null){
-
         $this->categoryid=$categoryid;
-         
         $this->cat =Category::find($categoryid);
     }
     public function render()
     {
-        $data = ['products'=>ProductDetails::Getcategory($this->categoryid)->online()->paginate($this->pg)];
+        $data = ['products'=>ProductDetails::Getcategory($this->categoryid)->online()->orderBy('updated_at','DESC')->paginate($this->pg)];
         return view('livewire.front.category.viewcategory',['data'=>$data])->layout('layouts.front-end.layout');
     }
 }
