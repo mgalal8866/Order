b30fd15e017aaa73550d0288b092a20d06621689
mgalal8866 <mgalal8866@gmail.com>
2023-10-13 23:59:48 +0300

    modified:   app/Http/Controllers/Api/V2/UserController.php

--- app/Http/Controllers/Api/V2/UserController.php
+++ app/Http/Controllers/Api/V2/UserController.php
@@ -13,53 +13,53 @@ use App\Repositoryinterface\UserRepositoryinterface;
 
 class UserController extends Controller
 {
-    private $userRepositry;
-    public function __construct(UserRepositoryinterface $userRepositry)
-    {
-        $this->userRepositry = $userRepositry;
-    }
-    public function checkphone($phone)
-    {
-        return $this->userRepositry->checkphone($phone);
-    }
+    // private $userRepositry;
+    // public function __construct(UserRepositoryinterface $userRepositry)
+    // {
+    //     $this->userRepositry = $userRepositry;
+    // }
+    // public function checkphone($phone)
+    // {
+    //     return $this->userRepositry->checkphone($phone);
+    // }
 
-    public function checkanswer(Request $request)
-    {
-        return $this->userRepositry->checkanswer($request);
-    }
-    public function register(RegisterUser $request)
-    {
-        $data = new UserResource($this->userRepositry->register($request->validated()));
-        return Resp($data, 'Success', 200, true);
-    }
+    // public function checkanswer(Request $request)
+    // {
+    //     return $this->userRepositry->checkanswer($request);
+    // }
+    // public function register(RegisterUser $request)
+    // {
+    //     $data = new UserResource($this->userRepositry->register($request->validated()));
+    //     return Resp($data, 'Success', 200, true);
+    // }
 
-    public function getuserdata()
-    {
-        return   $this->userRepositry->getuserdata();
+    // public function getuserdata()
+    // {
+    //     return   $this->userRepositry->getuserdata();
 
-    }
-    public function login(Request $request)
-    {
-        return   $this->userRepositry->login($request);
-    }
+    // }
+    // public function login(Request $request)
+    // {
+    //     return   $this->userRepositry->login($request);
+    // }
 
-    public function edit(Request $request)
-    {
-        return   $this->userRepositry->edit($request->all());
-    }
+    // public function edit(Request $request)
+    // {
+    //     return   $this->userRepositry->edit($request->all());
+    // }
 
-    protected function respondWithToken($token, $user = null)
-    {
-        return response()->json([
-            'data' => $user ?? '',
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            // 'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
-    }
-    public function question()
-    {
-        $question = question::get();
-        return Resp($question, 'success');
-    }
+    // protected function respondWithToken($token, $user = null)
+    // {
+    //     return response()->json([
+    //         'data' => $user ?? '',
+    //         'access_token' => $token,
+    //         'token_type' => 'bearer',
+    //         // 'expires_in' => auth()->factory()->getTTL() * 60
+    //     ]);
+    // }
+    // public function question()
+    // {
+    //     $question = question::get();
+    //     return Resp($question, 'success');
+    // }
 }
