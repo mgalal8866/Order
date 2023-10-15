d02297a546f40ff99df1352ce4528e33e43a018d
mgalal8866 <mgalal8866@gmail.com>
2023-10-15 05:41:07 +0300

    modified:   app/Http/Livewire/Front/User/Resetpassword.php

--- app/Http/Livewire/Front/User/Resetpassword.php
+++ app/Http/Livewire/Front/User/Resetpassword.php
@@ -5,14 +5,23 @@ namespace App\Http\Livewire\Front\User;
 use Livewire\Component;
 use App\Models\question;
 use App\Models\CateoryApp;
+use App\Models\User;
 
 class Resetpassword extends Component
 {
-    public $nashat, $answer1, $answer2, $question1_id, $question2_id, $question, $client_fhonewhats;
+    public $nashat, $answer1, $answer2, $question1, $question2,  $client_fhonewhats;
     public function mount()
     {
         $this->nashat = CateoryApp::get();
-        $this->question = question::get();
+
+    }
+    public function checkphone()
+    {
+        $user = User::where('client_fhonewhats', $this->client_fhonewhats)->with(['question1','question2'])->first();
+        if ($user != null) {
+            $this->question1 = $user->question1->question;
+            $this->question2 = $user->question2->question;
+        }
     }
     public function render()
     {
