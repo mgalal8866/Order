ce232eeaa13908c2c22c993e1fbbc5a238a7e082
mgalal8866 <mgalal8866@gmail.com>
2023-10-19 04:24:53 +0300

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -164,7 +164,7 @@ class SyncController extends Controller
             // $data = ['users_online' =>   clientsyncResource::collection(User::where('source_id', null)->get()) ?? [], 'results' => $results ?? [], 'errors' => $errors ?? []];
             $data = ['users_online' =>   clientsyncResource::collection(User::where('source_id', null)->get()) ?? [], 'results' => $results ?? [], 'errors' =>  []];
 
-            Log::error($data);
+            // Log::error($data);
             return  $data;
         } catch (\Exception $e) {
             logsync::create(['type' => "Error", 'data' => null,  'massage' =>  json_encode($e->getMessage())]);
