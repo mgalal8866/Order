f21fe43d9ac42a1f0bec63b3a96829d3d43b47a1
mgalal8866 <mgalal8866@gmail.com>
2023-10-19 23:43:50 +0300

    modified:   resources/views/livewire/front/compon/product.blade.php

--- resources/views/livewire/front/compon/product.blade.php
+++ resources/views/livewire/front/compon/product.blade.php
@@ -106,7 +106,7 @@
                     </h6>
                 </a>
                 <h6 class="name theme-color">
-                    @if($product->productheader->stock->count() > 1)
+                    @if($product->productheader->stock->count() > 0)
                     {{ $product->Qtystockapi($product->productheader->stock->sum('quantity')) }}
                     @else
                     غير متوفر
