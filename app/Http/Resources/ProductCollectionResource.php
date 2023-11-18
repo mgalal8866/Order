d6cef78964e9fcf4f9ef8332029e4e6e714f5c6d
mgalal8866 <mgalal8866@gmail.com>
2023-11-18 02:00:06 +0200

    modified:   app/Http/Resources/ProductCollectionResource.php

--- app/Http/Resources/ProductCollectionResource.php
+++ app/Http/Resources/ProductCollectionResource.php
@@ -9,8 +9,19 @@ class ProductCollectionResource extends ResourceCollection
 
     public function toArray($request)
     {
+         // $uniqueBrands = $this->collection
+        //     ->filter(function ($item) {
+        //         // Use the filter to exclude null values
+        //         return !is_null(data_get($item, 'productheader.brand'));
+        //     })->pluck('productheader.brand')->unique();
+        $uniqueBrands = $this->collection
+    ->pluck('productheader.brand')
+    ->filter()
+    ->unique();
+            // $uniqueBrands = $this->collection->pluck('productheader.brand')->unique();
+
         return [
-            'brands'  =>   BrandResource::collection($this->collection->pluck('productheader.brand')->unique()),
+            'brands'  =>   BrandResource::collection($uniqueBrands ),
             'product' => ProductDetailsResource::collection($this->collection),
             'pagination' => [
                 'total'        => $this->total(),
