00212719d5072ae7bd2df4aa255ccb5e731425d1
mgalal8866 <mgalal8866@gmail.com>
2023-11-22 05:57:37 +0200

    modified:   app/Http/Controllers/Api/V1/SyncController.php

--- app/Http/Controllers/Api/V1/SyncController.php
+++ app/Http/Controllers/Api/V1/SyncController.php
@@ -95,6 +95,8 @@ class SyncController extends Controller
     }
     function upload_deskapp()
     {
+        $re = $this->get_version();
+        return $re['data']['last_version'];
         $jsonFile = public_path('asset/update_desk/data.json');;
         $newData = [
             'url' => 'order.exe',
