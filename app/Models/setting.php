5fcf074693f2ad71f94ca839c272049753875bbe
mgalal8866 <mgalal8866@gmail.com>
2023-12-05 07:34:11 +0200

    modified:   app/Models/setting.php

--- app/Models/setting.php
+++ app/Models/setting.php
@@ -12,7 +12,19 @@ class setting extends Model
     protected $guarded = [];
     public function getLogoShopAttribute($val)
     {
-        return getimage($val,'logos');
+        return getimage($val, 'logos');
+
+        // $path = public_path('asset/images/logos/' . $val);
+        // if (File::exists($path)) {
+        //     return ($val !== null) ? asset('asset/images/logos/' . $val) : asset('asset/images/noimage.jpg');
+        // } else {
+        //     return asset('asset/images/noimage.jpg');
+        // }
+    }
+    public function getPhotoMainAttribute($val)
+    {
+        if ($val == null) return null;
+        return getimage($val, 'logos');
 
         // $path = public_path('asset/images/logos/' . $val);
         // if (File::exists($path)) {
