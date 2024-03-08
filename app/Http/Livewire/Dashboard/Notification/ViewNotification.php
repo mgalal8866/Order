41f2a35bb8e1a15662690aea768b3cca074f9195
mgalal8866 <mgalal8866@gmail.com>
2024-03-08 03:53:58 +0200

    modified:   app/Http/Livewire/Dashboard/Notification/ViewNotification.php

--- app/Http/Livewire/Dashboard/Notification/ViewNotification.php
+++ app/Http/Livewire/Dashboard/Notification/ViewNotification.php
@@ -74,7 +74,7 @@ class ViewNotification extends Component
         // dd($this->selectmultiuser);
         if ($this->selectactive == 0 && count($this->selectmultiuser) > 0) {
             $send = DB::table('users')->whereIn('id', $this->selectmultiuser)->where('fsm', '!=', null)->select('fsm')->pluck('fsm')->toArray();
-            $results =  notificationFCM($this->title, $this->body, $send,null,null,null,null,false);
+            $results =  notificationFCM($this->title, $this->body, $send );
         } elseif ($this->selectactive == 1) {
             // $send =   DB::table('users')->where('fsm', '!=', null)->select('fsm')->pluck('fsm')->toArray();
 
